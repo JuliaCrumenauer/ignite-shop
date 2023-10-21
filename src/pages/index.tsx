9102ff9edc14d81b72c153a6e71c4636ad03380d
Julia Crumenauer <julia.crumenauer@gmail.com>
2023-10-21 18:34:47 -0300

    formatando preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,9 @@ import { useKeenSlider } from 'keen-slider/react'
 
 import { HomeContainer, Product } from "../styles/pages/home"
 
-import camiseta1 from '../assets/tshirt1.svg'
-import camiseta2 from '../assets/tshirt2.svg'
-import camiseta3 from '../assets/tshirt3.svg'
-import camiseta4 from '../assets/tshirt4.svg'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -52,7 +47,7 @@ export default function Home({ products }: HomeProps) {
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -64,13 +59,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100),
     }
   })
 
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, //2 hours
   }
 }
