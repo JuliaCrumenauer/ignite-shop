b04b54ae315866239fac531e5616bd1eca738c2d
Julia Crumenauer <julia.crumenauer@gmail.com>
2023-10-23 21:46:21 -0300

    estrutura pg sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link"
+import { ImageContainer, SuccessContainer } from "../styles/pages/success"
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+        <h1>Compra efetuada!</h1>
+
+        <ImageContainer>
+
+        </ImageContainer>
+
+        <p>Uhuul Julia Crumenauer, sua Camiseta Beyond the Limits já está a caminho da sua casa.</p>
+
+        <Link href="">Voltar ao catálogo</Link>
+        </SuccessContainer>
     )
 }
