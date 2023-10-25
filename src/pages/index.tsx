0dee3637f74b3c2ba1f226f68ae3ab7a50cf0bd0
Julia Crumenauer <julia.crumenauer@gmail.com>
2023-10-24 21:27:27 -0300

    dados da compra no sucesso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 export default function Home({ products }: HomeProps) {
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,18 +1,51 @@
 import Link from "next/link"
 import { ImageContainer, SuccessContainer } from "../styles/pages/success"
+import Image from "next/image";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
 
-export default function Success() {
+interface SuccessProps {
+    costumerName: string,
+    product: {
+        name: string;
+        imageUrl: string;
+    }
+}
+
+export default function Success( {costumerName, product}: SuccessProps ) {
     return (
         <SuccessContainer>
         <h1>Compra efetuada!</h1>
 
         <ImageContainer>
-
+            <Image src={product.imageUrl} width={120} height={110} alt="" />
         </ImageContainer>
 
-        <p>Uhuul Julia Crumenauer, sua Camiseta Beyond the Limits já está a caminho da sua casa.</p>
+        <p>Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.</p>
 
         <Link href="">Voltar ao catálogo</Link>
         </SuccessContainer>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const sessionId = String(query.session_id);
+  
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items', 'line_items.data.price.product']
+    });
+  
+    const costumerName = session.customer_details.name;
+    const product = session.line_items.data[0].price.product as Stripe.Product;
+  
+    return {
+      props: {
+        costumerName,
+        product: {
+          name: product.name,
+          imageUrl: product.images[0]
+        }
+      }
+    }
+  }
